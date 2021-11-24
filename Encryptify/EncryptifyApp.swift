42f55c823b2145e1659f409428b458cda5093af9
Artur Rybak <arturybak@outlook.com>
2021-11-24 16:56:37 +0000

    Initial Commit

---
+++ Encryptify/EncryptifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncryptifyApp.swift
+//  Encryptify
+//
+//  Created by Artur Rybak on 24/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct EncryptifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
